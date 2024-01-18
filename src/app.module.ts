3a0655a98f1384c4e612d38cbaa9de907352997c
Sebastian Ramirez <sebastianramirez748@gmail.com>
2024-01-18 15:55:56 -0500

    Added logic and route to create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExperiencesModule } from './experiences/experiences.module';
 import { CommonModule } from './common/common.module';
 import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { FilesModule } from './files/files.module';
     ExperiencesModule,
     CommonModule,
     FilesModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import * as bcr from 'bcrypt';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcr.hashSync(password, 10),
+      });
+      await this.userRepository.save(user);
+
+      delete user.password;
+      return user;
+    } catch (error) {
+      this.handleDBError(error);
+    }
+  }
+
+  private handleDBError(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    console.log(error);
+    throw new InternalServerErrorException('Please check server logs');
+  }
+}
